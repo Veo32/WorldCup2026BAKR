8de083ed955d48df09c7ab0726911075ac118183
Veo32 <veo1593570@gmail.com>
2026-06-14 13:37:19 +0200

    fix: remove migrate from prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig } from "prisma/config";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-const adapter = new PrismaPg(pool);
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
-  migrate: {
-    adapter,
-  },
 });
